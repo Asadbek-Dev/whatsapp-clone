a19efced33b62c5b3f8c29d15c7eacaa48b903e0
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-05-15 16:56:34 +0500

    added material-ui/core and icons,sidebar styled

---
+++ src/components/Sidebar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import '../style/Sidebar.css'
+import { Avatar, IconButton } from '@material-ui/core';
+import { Chat, DonutLarge, MoreVert, SearchOutlined } from '@material-ui/icons';
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            <div className='sidebar__header'>
+                <Avatar />
+                <div className='sidebar__headerRight'>
+                    <IconButton>
+                        <DonutLarge />
+                    </IconButton>
+                    <IconButton>
+                        <Chat />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+            <div className='sidebar__search'>
+                <div className='sidebar__searchContainer'>
+                    <SearchOutlined />
+                    <input placeholder='search or start new chat' type='text' />
+                </div>
+
+            </div>
+            <div className='sidebar__chats'>
+                <SidebarChat />
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
