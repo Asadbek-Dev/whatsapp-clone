3d11344880441fd1f43cb65fdd018b304f9a6290
Asadbek-Dev <asadbek.dev1101@gmail.com>
2021-05-16 17:50:15 +0500

    styled chatside

---
+++ src/components/Chat.js
@@ -0,0 +1,44 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import React, { useEffect, useState } from 'react';
+import '../style/Chat.css'
+
+const Chat = () => {
+    const [seed, setSeed] = useState('');
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+    return (
+        <div className='chat'>
+            <div className='chat__header'>
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className='chat__headerInfo'>
+                    <h3>Room name</h3>
+                    <p>Last seen at ...</p>
+                </div>
+                <div className='chat__headerRight'>
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+            <div className='chat__body'>
+
+                <p className='chat__message chat__reciever'>
+                    <span className='chat__name'>Asadbek</span>
+                    Hey Guys
+                    <span className='chat__timestamp'>11:12pm</span>
+                </p>
+            </div>
+            <div className='chat__footer'></div>
+        </div>
+    )
+}
+
+export default Chat
